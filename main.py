f6bdebd1470de67fd01d81fe9cefc06179949ddb
scut-pan <984582466@qq.com>
2025-12-12 16:40:15 +0800

    feat: 学习响应类型JSONResponse、HTMLResponse、FileResponse和自定义响应类型，异常处理

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi import HTTPException
 
 # 创建 FastAPI 实例
 app = FastAPI()
@@ -43,3 +45,38 @@ class User(BaseModel):
 @app.post("/register")
 async def register(user: User):
     return user
+
+
+# 响应类型
+# 响应HTML内容
+@app.get("/html", response_class=HTMLResponse)
+async def get_html():
+    return "<h1>这是一级标题</h1>"
+
+
+# 响应File内容
+@app.get("/file", response_class=FileResponse)
+async def get_file():
+    path = "./files/bird.jpg"  # 文件路径
+    return FileResponse(path)
+
+
+# 自定义响应数据格式
+# 需求：新增接口 + 相应数据格式 id, title, content
+class News(BaseModel):
+    id: int
+    title: str
+    content: str
+    
+@app.get("/news/{id}", response_model=News)
+async def get_news(id: int):
+    # 异常处理
+    # 需求：按 id 查询新闻 → 1-6
+    id_list = [1, 2, 3, 4, 5, 6]
+    if id not in id_list:
+        raise HTTPException(status_code=404, detail="您查找的新闻不存在")
+    return {
+        "id": id,
+        "title": f"这是第{id}条新闻标题",
+        "content": f"这是第{id}条新闻内容"
+    }
